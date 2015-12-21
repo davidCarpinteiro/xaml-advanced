73b38f995a4bbc2d5577fc0eb25e91878599f38c
David Carpinteiro <david_carpinteiro@hotmail.com>
2015-12-21 13:25:45 +0000

    Problem with homework 3, extensions project was not included

---
+++ RestaurantManager.Extensions/BooleanToColorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace RestaurantManager.Extensions
+{
+    public class BooleanToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (bool)value ? Colors.Green : Colors.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RestaurantManager.Extensions/RightClickMessageDialogBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Popups;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickMessageDialogBehavior : DependencyObject, IBehavior
+    {
+        public string Message { get; set; }
+
+        public string Title { get; set; }
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is Page)
+            {
+                this.AssociatedObject = associatedObject;
+                (this.AssociatedObject as Page).RightTapped += Page_RightTapped;
+            }
+        }
+
+        private async void Page_RightTapped(object sender, RightTappedRoutedEventArgs e)
+        {
+            await new MessageDialog(Message, Title).ShowAsync();
+        }
+
+        public void Detach()
+        {
+            (this.AssociatedObject as Page).RightTapped -= Page_RightTapped;
+        }
+    }
+}
